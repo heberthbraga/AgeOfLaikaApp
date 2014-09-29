715f025045fca44f801671cebca60b6d396220fa
Heberth Braga <heberth@quimbik.com>
2014-09-29 16:00:51 -0300

    Adding age of laika remaining files

--- AgeOfLaikaApp/ViewController.swift
+++ AgeOfLaikaApp/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    
+    @IBOutlet weak var enterDogYearsTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func convertDogYearBtn(sender: UIButton) {
+        var dogYears:Double = Double((enterDogYearsTextField.text as NSString).doubleValue)
+        
+        if dogYears > 2 {
+            dogYears = (10.5 * 2) + ((dogYears - 2) * 4)
+        }else {
+            dogYears = dogYears * 10.5
+        }
+        
+        dogYearsLabel.text = "\(dogYears)" + " Human Years"
+    }
 }
-
